422b419502a66514cd1bcdfbc7a19151edfa96a4
Pratham-k-dev <pratham.k3007@gmail.com>
2025-08-15 17:34:44 +0530

    next_per_comment

--- arrays-strings-matrices-algos/next_perm.cpp
+++ arrays-strings-matrices-algos/next_perm.cpp
@@ -4,23 +4,26 @@ using namespace std;
 int * next_perm(vector <int> &vec){//O(n) 
  int n=vec.size();
     int i=n-1;
-    while(i>=1 && vec[i-1]>=vec[i]) i--;
+     // Step 1: Find the largest index i such that vec[i-1] < vec[i]
+    // (this identifies the pivot point where the next permutation changes)
+    while(i>=1 && vec[i-1]>=vec[i]) i--; 
 
 
 
     if(i){
-
+        // Step 2: Find the smallest number greater than vec[i-1]
         for(int j=n-1;j>=i;j--) {
             if(vec[j]>vec[i-1]) {
-                swap(vec[i-1],vec[j]);
+                swap(vec[i-1],vec[j]); 
                 break;
             }
 
         }
+         // Step 3: Reverse the suffix starting at i
         reverse(vec.begin()+i,vec.end());
         return &vec[0];
     }
-    else return nullptr; //reverse(vec.begin(),vec.end());
+    else return nullptr; // or reverse(vec.begin(),vec.end());
 }
 
 int fact(int n) {
