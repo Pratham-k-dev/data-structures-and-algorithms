3fe59fb9c01c52c4db4fece1135892efcd54ca09
Pratham-k-dev <pratham.k3007@gmail.com>
2025-10-09 21:52:44 +0530

    lcs

--- dynamic-programming/dp_on_strings/longest_common_subseq.cpp
+++ dynamic-programming/dp_on_strings/longest_common_subseq.cpp
@@ -42,8 +42,32 @@ using namespace std;
         return dp[n-1][m-1];
 
     }
+     int longestCommonSubsequence_1(string text1, string text2) { //tabulation , 1 based  indexed
+        int n=text1.size(),m=text2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+       
+        
 
-     int longestCommonSubsequence(string text1, string text2) { //space optimized
+        for(int i1=1;i1<=n;i1++){
+            for(int i2=1;i2<=m;i2++){
+                if(text1[i1-1]==text2[i2-1]){
+                     dp[i1][i2]=1+dp[i1-1][i2-1];
+                }
+                else{
+                    
+                     int x=dp[i1-1][i2];
+                     int y=dp[i1][i2-1];
+                    dp[i1][i2]=max(x,y);
+                }
+            }
+        }
+        return dp[n][m];
+
+    }
+
+
+
+     int longestCommonSubsequence3(string text1, string text2) { //space optimized
         int n=text1.size(),m=text2.size();
         
 
