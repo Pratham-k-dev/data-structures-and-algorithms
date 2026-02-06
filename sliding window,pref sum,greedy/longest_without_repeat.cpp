cefb27431a573c002028d63b7ea7d0a386e16878
Pratham-k-dev <pratham.k3007@gmail.com>
2026-02-06 11:18:54 +0530

    2point

--- sliding window,pref sum,greedy/longest_without_repeat.cpp
+++ sliding window,pref sum,greedy/longest_without_repeat.cpp
@@ -3,25 +3,41 @@
 using namespace std;
 class Solution {
 public:
-    int lengthOfLongestSubstring(string s) {
-           
-    int n=s.length();
-    int i=0;
-    int maxi=0;
-        unordered_set<char> st;
+    int lengthOfLongestSubstring(string s) {// method 1 using (char,count) map
+        int n=s.size();
+        unordered_map<char,int> mp;
+        int i=0;
         int j=0;
-    while(i<n){
+        int ans=0;
+        while(j<n){
+            while(i<j && mp[s[j]]){
+                mp[s[i]]--;
+             i++;
+            }
 
-        while( st.count(s[i])){
-            st.erase(s[j]);
+            ans=max(ans,j-i+1);
+            mp[s[j]]++;
             j++;
         }
-        st.insert(s[i]);
-        maxi=max(maxi,i-j+1);
-        i++;
+        return ans;
+        
     }
-    return maxi;
-    
+     int lengthOfLongestSubstring(string s) {//method 2 using (char,prev seen index) map
+        int n=s.size();
+        vector<int> hash(255,-1);
+        int i=0;
+        int j=0;
+        int ans=0;
+        while(j<n){
+            if(hash[s[j]]>=i){
+                i=hash[s[j]]+1;
+            }
+
+            ans=max(ans,j-i+1);
+            hash[s[j]]=j;
+            j++;
+        }
+        return ans;
         
     }
 };
