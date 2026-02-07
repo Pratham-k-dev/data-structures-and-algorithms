5731340d666cbfa880011c75d7efc86cc8ab923e
Pratham-k-dev <pratham.k3007@gmail.com>
2026-02-07 22:05:53 +0530

    rename

---
+++ sliding window, pref sum/cf.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+#define ll long long
+#define vi vector<int>
+#define vll vector<ll>
+#define pb push_back
+#define ft first
+#define sd second
+#define all(x) (x).begin(), (x).end()
+#define f(i,a,b) for(ll i=a;i<b;i++)
+#define tc() ll t; cin>>t; while(t--)
+#define endl '\n'
+using namespace std;
+
+void solve(){
+}
+int main (){
+ios::sync_with_stdio(false);
+cin.tie(0);
+tc(){
+    
+}
+    return 0;
+}
---
+++ sliding window, pref sum/tempCodeRunnerFile.cpp
@@ -0,0 +1,10 @@
+ int i=0;
+//    while(i<n && a[i]==i+1) i++;
+//    if(i==n){
+
+//        cout<<0<<endl;
+//        return;
+//    }
+//    int j=n-1;
+//    while(j>=0 && a[j]==j+1) j--;
+//     cout<<n-i-j<<endl;
